4337679d7f6abc435e2f36038ec6f4b966889123
Naman Mahajan <mahajannaman2020@gmail.com>
2025-03-09 01:38:54 +0530

    Payment page

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,56 +1,61 @@
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
-import GitHubProvider from 'next-auth/providers/github'
-import mongoose from 'mongoose'
-import User from '@/model/User'
-import Payement from '@/model/Payement'
-export const authoptions = NextAuth({
-  providers: [
-    // OAuth authentication providers...
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
-    // AppleProvider({
-    //   clientId: process.env.APPLE_ID,
-    //   clientSecret: process.env.APPLE_SECRET
-    // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_ID,
-    //   clientSecret: process.env.FACEBOOK_SECRET
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET
-    // }),
-    // // Passwordless / email sign in
-    // EmailProvider({
-    //   server: process.env.MAIL_SERVER,
-    //   from: 'NextAuth.js <no-reply@example.com>'
-    // }),
-  ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      if (account.provider == "github") {
-        const client = await mongoose.connect("mongodb://localhost:27017/chai")
-        const currentUser = User.findOne({ email: email })
-        if (!currentUser) {
-          const newUser = new User({
-            email: email,
-            username: email.split("@")[0],
-          })
-          await newUser.save()
-          user.name = newUser.username
-        }
-        else {
-          user.name = currentUser.username
-        }
-        return true;
-      }
-    }
-  }
-})
-export { authoptions as GET, authoptions as POST }
+// import AppleProvider from 'next-auth/providers/apple'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import GoogleProvider from 'next-auth/providers/google'
+// import EmailProvider from 'next-auth/providers/email'
+import GitHubProvider from "next-auth/providers/github";
+import mongoose from "mongoose";
+import connectDb from '@/db/connectDb';
+import User from '@/models/User';
+import Payment from '@/models/Payment';
+export const authoptions =  NextAuth({
+    providers: [
+      // OAuth authentication providers...
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+      }),
+    //   AppleProvider({
+    //     clientId: process.env.APPLE_ID,
+    //     clientSecret: process.env.APPLE_SECRET
+    //   }),
+    //   FacebookProvider({
+    //     clientId: process.env.FACEBOOK_ID,
+    //     clientSecret: process.env.FACEBOOK_SECRET
+    //   }),
+    //   GoogleProvider({
+    //     clientId: process.env.GOOGLE_ID,
+    //     clientSecret: process.env.GOOGLE_SECRET
+    //   }),
+    //   // Passwordless / email sign in
+    //   EmailProvider({
+    //     server: process.env.MAIL_SERVER,
+    //     from: 'NextAuth.js <no-reply@example.com>'
+    //   }),
+    ],
+    callbacks: {
+      async signIn({ user, account, profile, email, credentials }) {
+         if(account.provider == "github") { 
+          await connectDb()
+          // Check if the user already exists in the database
+          const currentUser =  await User.findOne({email: email}) 
+          if(!currentUser){
+            // Create a new user
+             const newUser = await User.create({
+              email: user.email, 
+              username: user.email.split("@")[0], 
+            })   
+          } 
+          return true
+         }
+      },
+      
+      async session({ session, user, token }) {
+        const dbUser = await User.findOne({email: session.user.email})
+        session.user.name = dbUser.username
+        return session
+      },
+    } 
+  })
+
+  export { authoptions as GET, authoptions as POST}
